d23a4c1786fe61270ca1dd8f23a04ab3e76ac388
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2025-04-05 17:34:39 -0400

    feat: add yaml support

--- main.py
+++ main.py
@@ -10,18 +10,26 @@ mcp = FastMCP("ast-grep")
 @mcp.tool()
 def find_code(
     project_folder: str = Field(description="The path to the project folder"),
-    pattern: str = Field(description="The ast-grep pattern to search for"),
+    pattern: str = Field(description="The ast-grep pattern to search for. Note the pattern must has valid AST structure."),
     language: str = Field(description="The language of the query", default=""),
 ) -> List[dict[str, Any]]:
-    """Find code in a project folder that matches the given ast-grep pattern"""
+    """
+    Find code in a project folder that matches the given ast-grep pattern.
+    Pattern is good for simple and single-AST node result.
+    For more complex usage, please use YAML by `find_code_by_rule`.
+    """
     return run_ast_grep_command(pattern, project_folder, language)
 
 @mcp.tool()
 def find_code_by_rule(
     project_folder: str = Field(description="The path to the project folder"),
-    yaml: str = Field(description="The ast-grep pattern to search for"),
+    yaml: str = Field(description="The ast-grep YAML rule to search. It must have id, language, rule fields."),
     ) -> List[dict[str, Any]]:
-    """Find code using ast-grep's YAML rule in a project folder"""
+    """
+    Find code using ast-grep's YAML rule in a project folder.
+    YAML rule is more powerful than simple pattern and can perform complex search like find AST inside/having another AST.
+    It is a more advanced search tool than the simple `find_code`.
+    """
     return run_ast_grep_yaml(yaml, project_folder)
 
 def run_ast_grep_command(pattern: str, project_folder: str, language: Optional[str]) -> List[dict[str, Any]]:
